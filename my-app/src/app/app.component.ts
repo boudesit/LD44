d0c65a6ab4e7310171d4f5fd34a4b58a4ab0bd4c
boudesit <boudesit@gmail.com>
2019-03-31 20:53:24 +0200

    Real initial project

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import {environment} from '../environments/environment';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'my-app';
+  public game: Phaser.Game;
+
+
+  public readonly gameConfig: GameConfig = {
+    title: environment.title,
+    version: environment.version,
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: {
+      create: function () {
+        this.cameras.main.startFollow(this.add.text(0, 0, 'It\'s the first screen of the game').setOrigin(0.5), false);
+      }
+    }
+};
+
+  public onGameReady(game: Phaser.Game): void {
+    this.game = game;
+  }
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { PhaserModule } from 'phaser-component-library';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PhaserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
